45d3b317d400845608f2f6febf2f0ce77caf9293
ithelor <61781601+Ithelor@users.noreply.github.com>
2022-05-20 00:20:07 +1000

    ci: generalize

--- src/components/FancyButton/FancyButton.tsx
+++ src/components/FancyButton/FancyButton.tsx
@@ -1,21 +1,21 @@
 import { motion } from 'framer-motion'
-import useModal from 'hooks/useModal'
 
 import styles from './FancyButton.module.scss'
 
-const FancyButton = () => {
-  const { isModalOpen, openModal, closeModal } = useModal()
-
-  return (
-    <motion.button
-      whileHover={{ scale: 1.1 }}
-      whileTap={{ scale: 0.9 }}
-      className={styles.container}
-      onClick={() => (isModalOpen ? closeModal() : openModal())}
-    >
-      Авторизация
-    </motion.button>
-  )
+interface IFancyButton {
+  title: string
+  onClick: () => void
 }
+
+const FancyButton = (props: IFancyButton) => (
+  <motion.button
+    whileHover={{ scale: 1.1 }}
+    whileTap={{ scale: 0.9 }}
+    className={styles.container}
+    onClick={props.onClick}
+  >
+    {props.title}
+  </motion.button>
+)
 
 export default FancyButton
